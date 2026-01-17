dcab210c05e7411998a74d1321ebd9bd53c1247e
Rodrigo Valentim <rodrigo.valentim.alves@outlook.com>
2026-01-16 22:03:07 -0300

    feat: adiciona endpoint de busca por id

--- src/main/java/com/desenvolvimento/transito/api/controller/ProprietarioController.java
+++ src/main/java/com/desenvolvimento/transito/api/controller/ProprietarioController.java
@@ -2,30 +2,34 @@ package com.desenvolvimento.transito.api.controller;
 
 
 import com.desenvolvimento.transito.domain.model.Proprietario;
+import com.desenvolvimento.transito.domain.repository.ProprietarioRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
-    @GetMapping("/proprietarios")
+    @Autowired
+    private final ProprietarioRepository proprietarioRepository;
+
+    @GetMapping()
     public List<Proprietario> listar() {
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("João");
-        proprietario1.setTelefone("34 99999-1111");
-        proprietario1.setEmail("joaodascouves@algaworks.com");
-
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Maria");
-        proprietario2.setTelefone("11 97777-1111");
-        proprietario2.setEmail("mariadasilva@algaworks.com");
-
-        return Arrays.asList(proprietario1, proprietario2);
+        return proprietarioRepository.findAll();
     }
 
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.noContent().build());
+    }
 }
