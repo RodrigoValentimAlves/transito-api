1bba08d539aa26e9b288d2fac76647d889f2416d
Rodrigo Valentim <rodrigo.valentim.alves@outlook.com>
2026-03-11 21:08:32 -0300

    feat: add regra de negocio para apreender e remover apreensao

--- src/main/java/com/desenvolvimento/transito/domain/model/Veiculo.java
+++ src/main/java/com/desenvolvimento/transito/domain/model/Veiculo.java
@@ -1,5 +1,6 @@
 package com.desenvolvimento.transito.domain.model;
 
+import com.desenvolvimento.transito.domain.exception.NegocioException;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -41,5 +42,31 @@ public class Veiculo {
         autuacao.setVeiculo(this);
         getAutuacoes().add(autuacao);
         return autuacao;
+    }
+
+    public void apreender() {
+        if (estaApreendido()) {
+            throw new NegocioException("Veículo já está apreendido.");
+        }
+
+        setStatus(StatusVeiculo.APREENDIDO);
+        setDataApreensao(OffsetDateTime.now());
+    }
+
+    public boolean estaApreendido() {
+        return StatusVeiculo.APREENDIDO.equals(getStatus());
+    }
+
+    public void removerApreensao() {
+        if (naoEstaApreendido()) {
+            throw new NegocioException("Veículo não está apreendido.");
+        }
+
+        setStatus(StatusVeiculo.REGULAR);
+        setDataApreensao(null);
+    }
+
+    public boolean naoEstaApreendido() {
+        return !estaApreendido();
     }
 }
